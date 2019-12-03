5d6e2634c7558968669639af02f257c99af7ddea
Jukka <joku@jossain.com>
2019-12-03 18:07:54 +0200

    Commit frontend

---
+++ build/precache-manifest.042f3ec627310f8c0e362043e119d9db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9bbcda1e88331abaaca3ddc17fa236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9463754863bc807c4578",
+    "url": "/static/css/main.1509b48e.chunk.css"
+  },
+  {
+    "revision": "1db49b975bb63ee2cbcd",
+    "url": "/static/js/2.b1580c2d.chunk.js"
+  },
+  {
+    "revision": "9463754863bc807c4578",
+    "url": "/static/js/main.23ed194e.chunk.js"
+  },
+  {
+    "revision": "336fb74f0a8ba4df2603",
+    "url": "/static/js/runtime-main.a19f6a7c.js"
+  }
+]);
---
+++ build/precache-manifest.064ebbb620039d0945cc7554b64ff0ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93c2eda9b7c5fed55c19eae2972e616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397a64de8291ef782caa",
+    "url": "/static/css/main.1509b48e.chunk.css"
+  },
+  {
+    "revision": "1db49b975bb63ee2cbcd",
+    "url": "/static/js/2.b1580c2d.chunk.js"
+  },
+  {
+    "revision": "397a64de8291ef782caa",
+    "url": "/static/js/main.37876558.chunk.js"
+  },
+  {
+    "revision": "336fb74f0a8ba4df2603",
+    "url": "/static/js/runtime-main.a19f6a7c.js"
+  }
+]);
